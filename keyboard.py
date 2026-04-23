73692876ddeccd7f845f2716426b92e06ec02357
matheussolanodelasala <matheussolanodelasala@gmail.com>
2026-04-23 17:51:35 -0500

    Update keyboard.py

--- keyboard.py
+++ keyboard.py
@@ -2,7 +2,7 @@ from pynput.keyboard import Listener
 
 def al_presionar(tecla):
     with open("log.txt", "a") as archivo:
-        # Registra la tecla tal cual la recibe el sistema
+
         archivo.write(f"{tecla} ")
 
 print("Grabando teclas... (Presiona Ctrl+C para salir)")
